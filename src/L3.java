775163ac111fc994d151963d7e1b409acda72c05
jericholeen <jleenGIT16>
2019-11-27 14:39:04 +0300

    test commit

---
+++ src/L3.java
@@ -0,0 +1,19 @@
+import java.sql.SQLTransactionRollbackException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class L3 {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+        met();
+    }
+
+    private static void met() {
+        System.out.print("TEST");
+    }
+
+}
